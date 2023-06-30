d7f6ddf8c28ab523772b94e47826bb8857065b94
Sivasankari S <136449856+sivasankari3523@users.noreply.github.com>
2023-06-30 12:31:23 +0530

    Update projectsData.js

--- src/components/Projects/projectsData.js
+++ src/components/Projects/projectsData.js
@@ -12,43 +12,43 @@ export const projectData = [
     id: 1,
     image: quotes,
     transitionImage: quotes,
-    title: "A quotes web app",
-    liveLink: "https://mugambi645.github.io/Quotes/",
+    title: "A quotes web app with react",
+   
   },
   {
     id: 2,
     image: unsplash,
     transitionImage: unsplash,
     title: "Unsplash images with react",
-    liveLink: "https://mellifluous-haupia-98b8e3.netlify.app/",
+   
   },
   {
     id: 3,
     image: ecommerce,
     transitionImage: ecommerce,
-    title: "Ecommerce with django",
-    liveLink: "https://ecommerce-project2.herokuapp.com/",
+    title: "Ecommerce with react",
+    
   },
   {
     id: 4,
     image: fylo,
     transitionImage: fylo,
     title: "Fylo landing page with react",
-    liveLink: "https://fylo-nu-ten.vercel.app/",
+    
   },
   {
     id: 5,
     image: meme,
     transitionImage: meme,
     title: "React meme generator ",
-    liveLink: "https://meme-generator-nine-eta.vercel.app/",
+   
   },
   {
     id: 6,
     image: ent,
     transitionImage: ent,
     title: "React entertainment web app",
-    liveLink: "https://react-entertainment-hub-lake.vercel.app/",
+   
   },
   
 ];
